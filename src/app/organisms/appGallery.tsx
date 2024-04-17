c6325684a2911680b7f3aee597f505dbf46f4ce5
Moritomo7315 <tennis1love7@gmail.com>
2024-04-17 23:12:08 +0900

    #1 add link when clicking app card

--- src/app/organisms/appGallery.tsx
+++ src/app/organisms/appGallery.tsx
@@ -1,23 +1,30 @@
+import Link from "next/link";
 import AppGalleryCard from "../molecules/appGalleryCard";
 
 export default function AppGallery() {
   return (
     <div className="grid lg:grid-cols-3 md:grid-cols-2 lg:gap-16">
-      <AppGalleryCard
-        imgPath="/thumbnail.png"
-        alt="アプリ１のイメージです"
-        title="アプリ1"
-      />
-      <AppGalleryCard
-        imgPath="/thumbnail.png"
-        alt="アプリ１のイメージです"
-        title="アプリ1"
-      />
-      <AppGalleryCard
-        imgPath="/thumbnail.png"
-        alt="アプリ１のイメージです"
-        title="アプリ1"
-      />
+      <Link href="#">
+        <AppGalleryCard
+          imgPath="/thumbnail.png"
+          alt="アプリ１のイメージです"
+          title="アプリ1"
+        />
+      </Link>
+      <Link href="#">
+        <AppGalleryCard
+          imgPath="/thumbnail.png"
+          alt="アプリ１のイメージです"
+          title="アプリ1"
+        />
+      </Link>
+      <Link href="#">
+        <AppGalleryCard
+          imgPath="/thumbnail.png"
+          alt="アプリ１のイメージです"
+          title="アプリ1"
+        />
+      </Link>
     </div>
   );
 }
