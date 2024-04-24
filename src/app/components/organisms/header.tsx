fc2c57ead13d0fdda99b7bac12b81c6906e9cc51
Moritomo7315 <tennis1love7@gmail.com>
2024-04-25 07:25:20 +0900

    #6 ヘッダーのリンクが全ページで使えるように修正

--- src/app/components/organisms/header.tsx
+++ src/app/components/organisms/header.tsx
@@ -2,26 +2,31 @@ import Link from "next/link";
 export default function Header() {
   return (
     <nav className="md:flex justify-between align-middle w-full bg-black p-6">
-      <div className="text-white">
-        <span className="font-semibold text-xl tracking-tight">
-          tomoya.mori
-        </span>
-      </div>
+      <Link
+        href="/"
+        className="block lg:inline-block lg:mt-0 text-white hover:text-red-500 mr-4"
+      >
+        <div className="text-white">
+          <span className="font-semibold text-xl tracking-tight">
+            tomoya.mori
+          </span>
+        </div>
+      </Link>
       <div className="flex">
         <Link
-          href="#app-gallery"
+          href="/#app-gallery"
           className="block lg:inline-block lg:mt-0 text-white hover:text-red-500 mr-4"
         >
           App Gallery
         </Link>
         <Link
-          href="#about-me"
+          href="/#about-me"
           className="block lg:inline-block lg:mt-0 text-white hover:text-red-500 mr-4"
         >
           About Me
         </Link>
         <Link
-          href="#contact"
+          href="/#contact"
           className="block lg:inline-block lg:mt-0 text-white hover:text-red-500 mr-4"
         >
           Contact
